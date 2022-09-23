448ab83ade975a9457bdd7bcd309231799a69047
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-09-23 14:47:44 +0100

    Video2

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import Footer from '../components/Footer'
+import NavBar from '../components/NavBar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      Hello Next
+        Hello Next
+        <Link href='/nested'><a>Go to nested</a></Link>
     </div>
   )
 }
