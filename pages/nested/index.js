a8780d22f11e73981c7d5e1793402b4f16f6d58f
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-09-27 21:03:00 +0100

    video6

--- pages/nested/index.js
+++ pages/nested/index.js
@@ -1,38 +1,37 @@
-import React from 'react'
-import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import React from "react";
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import Link from "next/link";
 
 export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
 
-  const response = await fetch('https://jsonplaceholder.typicode.com/users')
-
-  const json = await response.json()
-
+  const json = await response.json();
 
   return {
-    props: {users: json}
-  }
+    props: { users: json },
+  };
+};
 
-
-}
-
-const Nested = ({users}) => {
-
-  console.log(users)
+const Nested = ({ users }) => {
+  console.log(users);
   return (
     <>
       <List>
-      {
-        users && users.map((user) => {
-          return  <ListItem disablePadding key={user.id}>
-          <ListItemButton>
-            <ListItemText primary={user.name} />
-          </ListItemButton>
-        </ListItem>
-        })
-      }
+        {users &&
+          users.map((user) => {
+            return (
+              <Link href={`nested/${user.id}`} key={user.id}>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary={user.name} />
+                  </ListItemButton>
+                </ListItem>
+              </Link>
+            );
+          })}
       </List>
     </>
-  )
-}
+  );
+};
 
-export default Nested
+export default Nested;
